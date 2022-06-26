dd1af788d4f9de5cc8a79ceb7e1aed8668acabb3
DavitIshkhanyan <davitishkhanayn.01@mail.ru>
2022-06-27 00:05:16 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [data, setData] = useState([]);
+    const [fetchNum, setFetchNum] = useState('');
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            if (fetchNum) {
+                fetch(
+                    `https://jsonplaceholder.typicode.com/posts/${fetchNum}/comments`
+                )
+                    .then((response) => response.json())
+                    .then((json) => setData(json));
+            }
+        }, 1000);
+        return () => clearTimeout(timer);
+    }, [fetchNum]);
+    return (
+        <div className="App">
+            <input
+                value={fetchNum}
+                onChange={(e) => setFetchNum(e.target.value)}
+            />
+            {data.map((el) => {
+                return (
+                    <div key={el.id}>
+                        <span style={{ color: 'red' }}>{el.email}</span> :
+                        <span style={{ color: 'green' }}>{el.body}</span>
+                        <br />
+                    </div>
+                );
+            })}
+        </div>
+    );
 }
 
 export default App;
